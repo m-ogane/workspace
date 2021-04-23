8a6a9ba33e135fe686520cf9274d093060cab86b
m-ogane <masaruogane@gmail.com>
2021-04-23 15:15:15 +0900

    yarimasyo

--- resnnyuuP329/src/tekito_/Main.java
+++ resnnyuuP329/src/tekito_/Main.java
@@ -1,11 +1,17 @@
 package tekito_;
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println(1/0);
-		
+	for(int i = 1 ; i<=10;i++) {
+		System.out.println("数字を入力して下さい");
+		int[] 
+		int s = new Scanner(System.in).nextInt();
+		System.out.println(s*2);
+
+	}
+
 	}
 
 }
