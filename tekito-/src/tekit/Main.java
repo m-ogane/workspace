756ef353a4001966b66d38cc6beff05a7d27a8bd
m-ogane <masaruogane@gmail.com>
2021-05-07 15:24:37 +0900

    5/7

--- tekito-/src/tekit/Main.java
+++ tekito-/src/tekit/Main.java
@@ -1,9 +1,21 @@
 package tekit;
 
+import java.time.LocalDate;
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+//		LocalTime d = LocalTime.now();
+//		System.out.println(d);
+//		Scanner scanner = new Scanner(System.in);
+		LocalDate dateRandom;
+		dateRandom = LocalDate.now().minusDays(new Random().nextInt((int) dayPeriod));
+
+//	    String a = scanner.next();
+//	    System.out.println(a);
+
 
 	}
 
